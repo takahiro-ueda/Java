e93bafed59be89aad6c6aa149670cc3b8617dc4a
takahiro-ueda <pt25ue@gmail.com>
2020-07-26 15:50:05 +0900

    円周率に定数を利用する（エラー表示）

--- JavaLearn/src/Main.java
+++ JavaLearn/src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
   public static void main(String[] args) {
-    double pi = 3.14;
+    final double pi = 3.14;
     int pie = 5;
     System.out.println("半径" + pie + "cmのパイの面積は、");
     System.out.println(pie * pie * pi);
     System.out.println("パイの半径を倍にします");
-    pie = 10;
+    pi = 10;
     System.out.println("半径" + pie + "cmのパイの面積は、");
     System.out.println(pie * pie * pi);
   }
