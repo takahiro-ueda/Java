116e109d1fe4f4696bf414abcb9302d402777521
takahiro-ueda <pt25ue@gmail.com>
2020-07-26 15:00:02 +0900

    計算をする

--- JavaLearn/src/Main.java
+++ JavaLearn/src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     System.out.println("植田");
     System.out.println("25歳です");
     System.out.println("お酒が好きです");
+    System.out.println("31 + 31の計算をします");
+    System.out.println(31 + 31);
   }
 }
