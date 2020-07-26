ea4746ede6e7add7925ebf9b17e57422d09026dd
takahiro-ueda <pt25ue@gmail.com>
2020-07-27 00:16:26 +0900

    データより小さな型の変数に代入(エラー)

--- JavaLearn/src/lesson2.java
+++ JavaLearn/src/lesson2.java
@@ -1,8 +1,5 @@
 public class lesson2 {
   public static void main(String[] args) {
-    float f = 3;  /* float型の変数にint型を代入 */
-    double d = f; /* double型の変数にfloat型を代入 */
-    System.out.println(f);
-    System.out.println(d);
+    int i = 3.2; /* [大きな型の値を小さな型の変数に代入することは原則としてできない] */
   }
 }
