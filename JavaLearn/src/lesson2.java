286a67da4495b2716cde772039aedcab706a8513
takahiro-ueda <pt25ue@gmail.com>
2020-07-27 00:13:05 +0900

    値より大きな型の変数に代入

--- JavaLearn/src/lesson2.java
+++ JavaLearn/src/lesson2.java
@@ -1,9 +1,8 @@
 public class lesson2 {
   public static void main(String[] args) {
-    //++や--は、他の演算子と一緒に使わない！
-    int a = 10;
-    int b = 10;
-    System.out.println(++a + 50); /* ①変数aの値が1増える,②それに50を加えたものが画面に表示される*/
-    System.out.println(b++ + 50); /* ①変数bに50を加えたものが画面に表示される, ②変数bの値が1増える*/
+    float f = 3;  /* float型の変数にint型を代入 */
+    double d = f; /* double型の変数にfloat型を代入 */
+    System.out.println(f);
+    System.out.println(d);
   }
 }
