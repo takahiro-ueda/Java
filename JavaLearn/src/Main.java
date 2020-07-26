e7885aeaaed04d9961df232a2994230bae52db2c
takahiro-ueda <pt25ue@gmail.com>
2020-07-26 15:24:36 +0900

    コメント追加

--- JavaLearn/src/Main.java
+++ JavaLearn/src/Main.java
@@ -1,4 +1,8 @@
+/* サンプルプログラム　Main
+  開発者：　植田      作成日：　2020年7月     
+*/
 public class Main {
+  // ここからmainメソッド
   public static void main(String[] args) {
     System.out.println("RPG:スッキリ魔王討伐");
     System.out.println("Ver.0.1 by 湊");
