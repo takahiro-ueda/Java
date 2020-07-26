7d37fcdf1b7cbd6ab4516cde0bea178fc8c54015
takahiro-ueda <pt25ue@gmail.com>
2020-07-26 23:46:01 +0900

    インクリメント演算子

--- JavaLearn/src/lesson2.java
+++ JavaLearn/src/lesson2.java
@@ -1,6 +1,8 @@
 public class lesson2 {
   public static void main(String[] args) {
-    System.out.println("私の好きな記号は二重引用符(\")です");
-    System.out.println("洋服の値段は¥12000です");
+    int a;
+    a = 100;
+    a ++;
+    System.out.println(a);
   }
 }
