76da9448568de29211712967a33c74408bfabbd8
takahiro-ueda <pt25ue@gmail.com>
2020-07-27 00:34:40 +0900

    改行なし画面出力の命令

--- JavaLearn/src/lesson2.java
+++ JavaLearn/src/lesson2.java
@@ -1,8 +1,8 @@
 public class lesson2 {
   public static void main(String[] args) {
-    String name = "植田"; 
-    String message;
-    message = name + "さん、こんにちは";
-    System.out.println(message);
+    String name = "植田";
+    System.out.print("私の名前は");
+    System.out.print(name);
+    System.out.print("です");
   }
 }
