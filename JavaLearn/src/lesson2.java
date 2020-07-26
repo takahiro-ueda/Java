c723f1533f4a664f77339139348bc0d731324386
takahiro-ueda <pt25ue@gmail.com>
2020-07-26 23:53:43 +0900

    インクリメント・デクリメント演算子はひとつしか活用できない

--- JavaLearn/src/lesson2.java
+++ JavaLearn/src/lesson2.java
@@ -1,8 +1,9 @@
 public class lesson2 {
   public static void main(String[] args) {
-    int a;
-    a = 100;
-    a ++;
-    System.out.println(a);
+    //++や--は、他の演算子と一緒に使わない！
+    int a = 10;
+    int b = 10;
+    System.out.println(++a + 50); /* ①変数aの値が1増える,②それに50を加えたものが画面に表示される*/
+    System.out.println(b++ + 50); /* ①変数bに50を加えたものが画面に表示される, ②変数bの値が1増える*/
   }
 }
