dcec7157822b7d382470392f83ca938deb61e89c
takahiro-ueda <pt25ue@gmail.com>
2020-07-26 15:26:50 +0900

    変数宣言の文

--- JavaLearn/src/Main.java
+++ JavaLearn/src/Main.java
@@ -4,10 +4,8 @@
 public class Main {
   // ここからmainメソッド
   public static void main(String[] args) {
-    System.out.println("RPG:スッキリ魔王討伐");
-    System.out.println("Ver.0.1 by 湊");
-    System.out.println("<ただいま学習・制作中>");
-    System.out.println("プログラムを終了します");
-    
+    int age;
+    age = 30;
+    System.out.println(age);
   }
 }
