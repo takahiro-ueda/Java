f489448766d6c0f4263b0ec46d1b799096bba297
takahiro-ueda <pt25ue@gmail.com>
2020-07-27 00:24:11 +0900

    異なる型同士の算術演算

--- JavaLearn/src/lesson2.java
+++ JavaLearn/src/lesson2.java
@@ -1,6 +1,8 @@
 public class lesson2 {
   public static void main(String[] args) {
-    int age = (int)3.2; /* [余程の理由がない限り使わない] */
-    System.out.println(age);
+    double d = 8.5 / 2; /* 2(int型)を2.0(double型)に変更 */
+    long l = 5 + 2L;  /* 5(int型)を5L(long型)に変更 */
+    System.out.println(d);
+    System.out.println(l);
   }
 }
