19b400de7ba8a58f962a5c2d9525043dbed577d0
takahiro-ueda <pt25ue@gmail.com>
2020-07-27 10:32:06 +0900

    先ほどの修正

--- JavaLearn/src/lesson2.java
+++ JavaLearn/src/lesson2.java
@@ -2,6 +2,6 @@ public class lesson2 {
   public static void main(String[] args) {
     String age = "31";
     int n = Integer.parseInt(age);  /* 文字列を数値に変換する */
-    System.out.println("あなたは来年、" + (n + 1) + "際になりますね。" );
+    System.out.println("あなたは来年、" + (n + 1) + "歳になりますね。" );
   }
 }
