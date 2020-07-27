396c713506a2cd0966ea154afbfdb0fb118d3c44
takahiro-ueda <pt25ue@gmail.com>
2020-07-27 15:25:45 +0900

    キーボードから入力を受け付ける命令

--- JavaLearn/src/lesson2.java
+++ JavaLearn/src/lesson2.java
@@ -1,6 +1,9 @@
 public class lesson2 {
   public static void main(String[] args) {
-    int r = new java.util.Random().nextInt(90);  /* 文字列を数値に変換する */
-    System.out.println("あなたは多分、" + r + "歳ですね？" );
+    System.out.println("あなたの名前を入力してください。");
+    String name = new java.util.Scanner(System.in).nextLine();
+    System.out.println("あなたの年齢を入力してください。");
+    int age = new java.util.Scanner(System.in).nextInt(); 
+    System.out.println("ようこそ、" + age+ "歳の" + name + "さん" );
   }
 }
