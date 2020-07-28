aa5399709865253566b7cc909e2e7fb5c141d874
takahiro-ueda <pt25ue@gmail.com>
2020-07-28 11:38:37 +0900

    for文のループ変数iの内容を表示する

--- JavaLearn/src/lesson3.java
+++ JavaLearn/src/lesson3.java
@@ -1,7 +1,7 @@
 public class lesson3 {
   public static void main(String[] args) {
-    for ( int i = 0; i < 10; i++) { //ここで繰り返し回数を決める
-      System.out.println("こんにちは");
+    for ( int i = 0; i < 3; i++) { //ここで繰り返し回数を決める
+      System.out.println("現在" + (i + 1) + "周目→");
     }
   }
 }
