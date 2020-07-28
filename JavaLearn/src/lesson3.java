772e78e0b4fa57ece27eec97ce65cc3414f08a2f
takahiro-ueda <pt25ue@gmail.com>
2020-07-28 11:35:35 +0900

    基本的なfor文

--- JavaLearn/src/lesson3.java
+++ JavaLearn/src/lesson3.java
@@ -1,18 +1,7 @@
 public class lesson3 {
   public static void main(String[] args) {
-    System.out.println("あなたの運勢を占います");
-    int fortune = new java.util.Random().nextInt(5) + 1;
-    switch (fortune) {
-      case 1:
-      case 2:
-        System.out.println("いいね！");
-        break;
-      case 3:
-        System.out.println("普通です");
-        break;
-      case 4:
-      case 5:
-        System.out.println("うーーーーん...");
+    for ( int i = 0; i < 10; i++) { //ここで繰り返し回数を決める
+      System.out.println("こんにちは");
     }
   }
 }
