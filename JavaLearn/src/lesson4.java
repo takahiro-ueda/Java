421d9ce4b9a309416de5270b8c3f9dbab4299aaf
takahiro-ueda <pt25ue@gmail.com>
2020-07-28 22:44:01 +0900

    点数管理プログラム(for文の利用)

--- JavaLearn/src/lesson4.java
+++ JavaLearn/src/lesson4.java
@@ -1,8 +1,12 @@
 public class lesson4 {
   public static void main(String[] args) {
     int [] scores = {20, 30, 40, 50, 80};
+    int sum = 0;  //集計結果を入れるための変数を初期化して準備
     for (int i = 0; i < scores.length; i++) {
-      System.out.println(scores[i]);
+      sum += scores[i]; //1科目ずつ変数sumに合算する
     }
+    int avg = sum / scores.length;
+    System.out.println("合計点: " + sum);
+    System.out.println("平均点: " + avg);
   }
 }
