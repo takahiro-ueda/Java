f40429f9dccb5b257a1fef5d62024290c757bbd5
takahiro-ueda <pt25ue@gmail.com>
2020-07-28 16:01:09 +0900

    配列の要素に値を代入

--- JavaLearn/src/lesson4.java
+++ JavaLearn/src/lesson4.java
@@ -1,7 +1,8 @@
 public class lesson4 {
   public static void main(String[] args) {
-    int[] scores = new int[5];
-    int num = scores.length;
-    System.out.println("要素の数: " + num );
+    int[] scores;
+    scores = new int[5];
+    scores[1] = 30; //2つ目の要素scores[1]に代入
+    System.out.println(scores[1]);  //2つ目のscores[1]の中身を表示
   }
 }
