c31a044714d62855d0dfecbdf3aaf85688b81933
takahiro-ueda <pt25ue@gmail.com>
2020-07-28 15:53:28 +0900

    配列の長さを調べる

--- JavaLearn/src/lesson4.java
+++ JavaLearn/src/lesson4.java
@@ -1,5 +1,7 @@
 public class lesson4 {
   public static void main(String[] args) {
-    int[] scores = new int[5]; 
+    int[] scores = new int[5];
+    int num = scores.length;
+    System.out.println("要素の数: " + num );
   }
 }
