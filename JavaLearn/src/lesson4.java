48007d631e39617e3a5c54cc0321aec30721eb01
takahiro-ueda <pt25ue@gmail.com>
2020-07-28 23:23:56 +0900

    従来のfor文と拡張for文の比較

--- JavaLearn/src/lesson4.java
+++ JavaLearn/src/lesson4.java
@@ -2,30 +2,17 @@ import javax.sound.midi.Sequence;
 
 public class lesson4 {
   public static void main(String[] args) {
-    int [] seq = new int [10];
 
-    //塩基配列をランダムに生成  
-    for (int i = 0; i < seq.length; i++) {
-      seq[i] = new java .util.Random().nextInt(4);
-    }
-    
-    //生成した塩基配列の記号を表示
-    for (int i = 0; i < seq.length; i++) {
-      switch (seq[i]) {
-        case 0:
-          System.out.print("A ");
-          break;
-        case 1:
-          System.out.print("T ");
-          break;
-        case 2:
-          System.out.print("G ");
-          break;
-        case 3:
-          System.out.print("C ");
-          break;
-      }
-      //13~26行目のswitch文が長く、冗長です。
+    //従来のfor文
+    // int[] scores = {20, 30, 40, 50, 80};
+    // for (int i = 0; i < scores.length; i++) {
+    //   System.out.println(scores[i]);
+    // }
+
+    //拡張for文
+    int[] scores = {20, 30, 40, 50, 80};
+    for (int value : scores) {
+      System.out.println(value);
     }
   }
 }
