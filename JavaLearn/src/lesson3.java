a19c9def7dd3b076765bebbc2df594958517ee23
takahiro-ueda <pt25ue@gmail.com>
2020-07-28 10:45:38 +0900

    トイレの空きを待つ繰り返し処理

--- JavaLearn/src/lesson3.java
+++ JavaLearn/src/lesson3.java
@@ -1,11 +1,9 @@
 public class lesson3 {
   public static void main(String[] args) {
-    boolean tenki = true;
-    if (tenki == true) {
-      System.out.println("洗濯をします");
-      System.out.println("散歩をします");
-    } else {
-      System.out.println("DVDを見ます");
+    boolean doorClose = true;  //ここでtrueかfalseを代入
+    if (doorClose == true) {
+      System.out.println("ノックする");
+      System.out.println("1分待つ");
     }
   }
 }
