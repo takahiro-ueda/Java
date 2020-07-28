243d0fc68beb8eeff249e462f929975d0fd23552
takahiro-ueda <pt25ue@gmail.com>
2020-07-28 10:53:25 +0900

    波カッコを省略した記述

--- JavaLearn/src/lesson3.java
+++ JavaLearn/src/lesson3.java
@@ -1,9 +1,11 @@
 public class lesson3 {
   public static void main(String[] args) {
-    boolean doorClose = true;  //ここでtrueかfalseを代入
-    if (doorClose == true) {
-      System.out.println("ノックする");
-      System.out.println("1分待つ");
-    }
+    boolean tenki = true;  //ここでtrueかfalseを代入
+    if (tenki == true) {
+      System.out.println("洗濯をします");
+      System.out.println("散歩をします");
+    } else //1行しかないので波カッコは省略可能
+      System.out.println("DVDを見ます");
   }
 }
+//ブロックの波カッコを省略することは推奨されていない
