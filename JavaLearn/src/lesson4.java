1f4ad78794d06c4db978bc9ceeca1a9706ab8b55
takahiro-ueda <pt25ue@gmail.com>
2020-07-28 23:55:10 +0900

    nullの利用(エラー)

--- JavaLearn/src/lesson4.java
+++ JavaLearn/src/lesson4.java
@@ -2,9 +2,8 @@ import javax.sound.midi.Sequence;
 
 public class lesson4 {
   public static void main(String[] args) {
-    boolean judge = true;
-    if (judge == true) {
-      int[] array = {1, 2, 3};  //ifブロック内で配列を作成
-    }
+    int[] array = {1, 2, 3};
+    array = null; //配列変数arrayにnullを代入
+    array[0] = 10;
   }
 }
