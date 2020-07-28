07b20c5abf54f34798bb6a1fa6824f97b319cff6
takahiro-ueda <pt25ue@gmail.com>
2020-07-28 23:14:42 +0900

    DNAの記号をランダムに表示する

--- JavaLearn/src/lesson4.java
+++ JavaLearn/src/lesson4.java
@@ -1,13 +1,31 @@
+import javax.sound.midi.Sequence;
+
 public class lesson4 {
   public static void main(String[] args) {
-    int [] scores = {20, 30, 40, 50, 80};
-    int count = 0;  
-    for (int i = 0; i < scores.length; i++) {
-      //条件に合致する要素があればカウントする
-      if (scores[i] >= 50) {
-        count++;
-      }
+    int [] seq = new int [10];
+
+    //塩基配列をランダムに生成  
+    for (int i = 0; i < seq.length; i++) {
+      seq[i] = new java .util.Random().nextInt(4);
     }
-    System.out.println("50点以上の科目の数は" + count);
+    
+    //生成した塩基配列の記号を表示
+    for (int i = 0; i < seq.length; i++) {
+      switch (seq[i]) {
+        case 0:
+          System.out.print("A ");
+          break;
+        case 1:
+          System.out.print("T ");
+          break;
+        case 2:
+          System.out.print("G ");
+          break;
+        case 3:
+          System.out.print("C ");
+          break;
+      }
+      //13~26行目のswitch文が長く、冗長です。
+    }
   }
 }
