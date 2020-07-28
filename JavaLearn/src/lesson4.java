dd67ca411edc4e7fa2e11fe11af092a15d991e21
takahiro-ueda <pt25ue@gmail.com>
2020-07-28 23:52:18 +0900

    ガベージコレクション

--- JavaLearn/src/lesson4.java
+++ JavaLearn/src/lesson4.java
@@ -2,10 +2,9 @@ import javax.sound.midi.Sequence;
 
 public class lesson4 {
   public static void main(String[] args) {
-    int [] arrayA = {1, 2, 3};  //arrayA[0]は1
-    int [] arrayB;
-    arrayB = arrayA;
-    arrayB[0] = 100;  //arrayB[0]に100を代入
-    System.out.println(arrayA[0]);  //arrayA[0]を表示すると・・・
+    boolean judge = true;
+    if (judge == true) {
+      int[] array = {1, 2, 3};  //ifブロック内で配列を作成
+    }
   }
 }
