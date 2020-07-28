b55adf5559ce03d1992c38dc6f1c78d81c1f1ada
takahiro-ueda <pt25ue@gmail.com>
2020-07-28 11:44:10 +0900

    for文のループを2重にして九九の表を出力する

--- JavaLearn/src/lesson3.java
+++ JavaLearn/src/lesson3.java
@@ -1,7 +1,11 @@
 public class lesson3 {
   public static void main(String[] args) {
-    for ( int i = 0; i < 3; i++) { //ここで繰り返し回数を決める
-      System.out.println("現在" + (i + 1) + "周目→");
+    for ( int i = 1; i < 10; i++) { //ここで繰り返し回数を決める
+      for ( int j = 1; j < 10; j++) {
+        System.out.println(i * j); //掛け算の結果を出力
+        System.out.println(""); //空白を出力
+      }
+      System.out.println(""); //改行を出力
     }
   }
 }
