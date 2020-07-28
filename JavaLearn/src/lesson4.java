9cf9987d9e676f04a18859180b2dfcd84555046e
takahiro-ueda <pt25ue@gmail.com>
2020-07-28 23:48:40 +0900

    配列理解　実装結果

--- JavaLearn/src/lesson4.java
+++ JavaLearn/src/lesson4.java
@@ -2,17 +2,10 @@ import javax.sound.midi.Sequence;
 
 public class lesson4 {
   public static void main(String[] args) {
-
-    //従来のfor文
-    // int[] scores = {20, 30, 40, 50, 80};
-    // for (int i = 0; i < scores.length; i++) {
-    //   System.out.println(scores[i]);
-    // }
-
-    //拡張for文
-    int[] scores = {20, 30, 40, 50, 80};
-    for (int value : scores) {
-      System.out.println(value);
-    }
+    int [] arrayA = {1, 2, 3};  //arrayA[0]は1
+    int [] arrayB;
+    arrayB = arrayA;
+    arrayB[0] = 100;  //arrayB[0]に100を代入
+    System.out.println(arrayA[0]);  //arrayA[0]を表示すると・・・
   }
 }
