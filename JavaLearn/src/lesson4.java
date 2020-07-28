f52ab9645cb8b652fcba1b77f09a91ebdcd5dc34
takahiro-ueda <pt25ue@gmail.com>
2020-07-29 00:27:07 +0900

    練習問題

--- JavaLearn/src/lesson4.java
+++ JavaLearn/src/lesson4.java
@@ -2,8 +2,39 @@ import javax.sound.midi.Sequence;
 
 public class lesson4 {
   public static void main(String[] args) {
-    int[][] scores = {{40, 50, 60}, {80, 60, 80}};  //このような初期化が可能
-    System.out.println(scores.length);  //2が出力される
-    System.out.println(scores[0].length); //3が出力される
+    /*練習問題4-1
+    int[] points = new int[4];  //int型の値を4個まとめて格納できる配列points
+    double[] weights = new double[5]; //double型の値を5個まとめて格納できる配列weights
+    boolean[] answers = new boolean[3]; //boolean型の値を3つまとめて格納できるanswers
+    String[] names = new String[3]; //String型の値を3つまとめて格納できる配列names
+    */
+
+    /* 練習問題4-2
+    int[] moneyList = {121902, 8302, 55100};
+    for (int i = 0; i < moneyList.length; i++) {
+      System.out.println(moneyList[i]);
+    }
+    for (int m : moneyList) {
+      System.out.println(m);
+    }
+    */
+
+    /* 練習問題4-4
+    //(1)配列の準備
+    int[] numbers = {3, 4, 9};
+
+    //(2)メッセージの表示
+    System.out.println("1桁の数字を入力してください");
+
+    //(3)キーボードからの数字入力
+    int input = new java.util.Scanner(System.in).nextInt();
+
+    //(4)配列を回しながら判定
+    for (int n : numbers) {
+      if (n == input) {
+        System.out.println("アタリ！！！！！！！");
+      }
+    }
+    */
   }
 }
