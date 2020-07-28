5525a24abd1d4fef883232f131a9d7c72ab977d3
takahiro-ueda <pt25ue@gmail.com>
2020-07-28 11:32:16 +0900

    あえてbreak文を書かない

--- JavaLearn/src/lesson3.java
+++ JavaLearn/src/lesson3.java
@@ -1,20 +1,18 @@
 public class lesson3 {
   public static void main(String[] args) {
     System.out.println("あなたの運勢を占います");
-    int fortune = 1;
+    int fortune = new java.util.Random().nextInt(5) + 1;
     switch (fortune) {
       case 1:
-        System.out.println("大吉");
-        //ここにbreak;を入れ忘れている（条件に一致するcaseラベルまで処理をジャンプさせる命令）
       case 2:
-        System.out.println("中吉");
+        System.out.println("いいね！");
         break;
       case 3:
-        System.out.println("吉");
+        System.out.println("普通です");
         break;
       case 4:
-        System.out.println("凶");
-        break;
+      case 5:
+        System.out.println("うーーーーん...");
     }
   }
 }
