3b958120c8a3323e1ddc9afebbd1da30ba150682
takahiro-ueda <pt25ue@gmail.com>
2020-07-29 00:04:28 +0900

    親配列と子配列の要素数を表示

--- JavaLearn/src/lesson4.java
+++ JavaLearn/src/lesson4.java
@@ -2,13 +2,8 @@ import javax.sound.midi.Sequence;
 
 public class lesson4 {
   public static void main(String[] args) {
-    int[][] scores = new int[2][3];
-    scores[0][0] = 40;
-    scores[0][1] = 50;
-    scores[0][2] = 60;
-    scores[1][0] = 80;
-    scores[1][1] = 60;
-    scores[1][2] = 70;
-    System.out.println(scores[1][1]);
+    int[][] scores = {{40, 50, 60}, {80, 60, 80}};  //このような初期化が可能
+    System.out.println(scores.length);  //2が出力される
+    System.out.println(scores[0].length); //3が出力される
   }
 }
