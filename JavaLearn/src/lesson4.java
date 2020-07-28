9db79cdd652eff8a437b90cf30b5b370295aa54a
takahiro-ueda <pt25ue@gmail.com>
2020-07-28 15:50:46 +0900

    配列の作成手順(Step1,Step2を同時に行う)

--- JavaLearn/src/lesson4.java
+++ JavaLearn/src/lesson4.java
@@ -1,6 +1,5 @@
 public class lesson4 {
   public static void main(String[] args) {
-    int[] scores; //int型の要素を代入できる配列変数scoresを用意([]が必要)
-    scores = new int[5];  //int型の要素を5つ作成してscoresに代入し、配列scoresの完成
+    int[] scores = new int[5]; 
   }
 }
