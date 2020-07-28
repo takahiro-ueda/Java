fbb84386cb2c06f679061f0189760148bdc15034
takahiro-ueda <pt25ue@gmail.com>
2020-07-28 16:03:44 +0900

    配列初期化されていない変数を利用(エラー)

--- JavaLearn/src/lesson4.java
+++ JavaLearn/src/lesson4.java
@@ -1,8 +1,6 @@
 public class lesson4 {
   public static void main(String[] args) {
-    int[] scores;
-    scores = new int[5];
-    scores[1] = 30; //2つ目の要素scores[1]に代入
-    System.out.println(scores[1]);  //2つ目のscores[1]の中身を表示
+    int x;
+    System.out.println(x) ;  //xが初期化されていないのでコンパイルエラーになる
   }
 }
