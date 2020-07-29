b6bbb7783a2e8ec1b6673ae359cbcd259f865c1d
takahiro-ueda <pt25ue@gmail.com>
2020-07-29 11:54:34 +0900

    mainメソッド以外からメソッドを呼び出す

--- JavaLearn/src/lesson5.java
+++ JavaLearn/src/lesson5.java
@@ -1,4 +1,5 @@
 public class lesson5 {
+  /*
   public static void main(String[] args) {
     System.out.println("メソッドを呼び出します");
     hello();  //helloメソッドを呼び出す
@@ -6,5 +7,16 @@ public class lesson5 {
   }
   public static void hello() {
     System.out.println("湊さん、こんにちは！！！！");
+  }
+  */
+  public static void methodA() {
+    System.out.println("methodA");
+    methodB();  //methodB メソッドの呼び出し
+  }
+  public static void methodB() {
+    System.out.println("methodB");
+  }
+  public static void main(String[] args) {
+    methodA();  //methodA メソッドの呼び出し
   }
 }
