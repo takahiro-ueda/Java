28c2fac1956595454ae2a3927c482413bb44a391
takahiro-ueda <pt25ue@gmail.com>
2020-07-29 11:40:51 +0900

    メソッドの呼び出し

--- JavaLearn/src/lesson5.java
+++ JavaLearn/src/lesson5.java
@@ -1,5 +1,10 @@
 public class lesson5 {
-  public static void main() {
-    System.out.println("植田さん、こんにちは！！！");
+  public static void main(String[] args) {
+    System.out.println("メソッドを呼び出します");
+    hello();  //helloメソッドを呼び出す
+    System.out.println("メソッドの呼び出しが終わりました");
+  }
+  public static void hello() {
+    System.out.println("湊さん、こんにちは！！！！");
   }
 }
