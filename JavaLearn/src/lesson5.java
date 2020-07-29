8e709cc9e89a597e52f679785e8526f35850ce4e
takahiro-ueda <pt25ue@gmail.com>
2020-07-29 16:31:59 +0900

    引数に配列を受け取るメソッドの利用

--- JavaLearn/src/lesson5.java
+++ JavaLearn/src/lesson5.java
@@ -1,15 +1,13 @@
 public class lesson5 {
-  //1つ目のaddメソッド
-  public static int add(int x, int y) { //1つ目のaddメソッド
-    return x + y;
-  }
-  //2つ目のaddメソッド
-  public static int add(int x, int y, int z) {  //2つ目のaddメソッド
-    return x + y + z;
+  //int型配列を受け取り、すべての要素を表示するメソッド
+  public static void printArray(int[] array) {  //int[]は引数に配列型を渡す
+    for (int element : array) {
+      System.out.println(element);
+    }
   }
   public static void main(String[] args) {
-    System.out.println("10+20= " + add(10, 20)); //1つ目のaddメソッドが呼び出される
-    System.out.println("10+20+30= " + add(10, 20, 30));  //2つ目のaddメソッドが呼び出される
+    int[] array = {1, 2, 3};
+    printArray(array);  //配列を渡す
   }
-  //仮引数の型だけでなく、個数が違う場合もオーバーロードできます
+  //printArrayメソッドに引数として渡しているのは、「配列まるごと」ではなくアドレス情報のみであることに注意
 }
