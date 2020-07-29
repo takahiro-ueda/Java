6970e755876071e39af20d769c3eab576e831ea0
takahiro-ueda <pt25ue@gmail.com>
2020-07-29 12:25:47 +0900

    戻り値の例

--- JavaLearn/src/lesson5.java
+++ JavaLearn/src/lesson5.java
@@ -1,21 +1,10 @@
 public class lesson5 {
-  // public static void main(String[] args) {
-  //   add(100, 20); //100と20を渡してaddメソッドを呼び出す
-  //   add(200, 50); //200と50を渡してaddメソッドを呼び出す
-  // }
-  // //複数の値を受け取るaddメソッド
-  // public static void add(int x, int y) {
-  //   int ans = x + y;
-  //   System.out.println(x + "+" + y + "=" + ans);
-  // }
+  public static int add(int x, int y) {
+    int ans = x + y;
+    return ans;
+  }
   public static void main(String[] args) {
-    int x = 100;  //addメソッドで使用するつもり
-    int y = 10; //addメソッドで使用するつもり
-    add();  addメソッドを呼び出す
+    int ans = add(100, 10);
+    System.out.println("100 + 10 = " + ans);
   }
-  public static void add() {
-    int ans = x + y;  //ここで使用するつもり(エラー)
-    System.out.println(x + "+" + y + "=" + ans);
-  }
-  //mainメソッド内で宣言した変数xとyは、mainメソッドのブロックの中でしか使用できない
 }
