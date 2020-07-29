d668a79f4fbf3d136f10c6c4b288463ddd7dd65a
takahiro-ueda <pt25ue@gmail.com>
2020-07-29 16:12:14 +0900

    オーバーロード(引数の型が異なる場合)

--- JavaLearn/src/lesson5.java
+++ JavaLearn/src/lesson5.java
@@ -1,10 +1,19 @@
 public class lesson5 {
+  //1つ目のaddメソッド
   public static int add(int x, int y) {
-    int ans = x + y;
-    return ans;
+    return x + y;
+  }
+  //2つ目のaddメソッド
+  public static double add(double x, double y) {
+    return x + y;
+  }
+  //3つ目のaddメソッド
+  public static String add(String x, String y) {
+    return x + y;
   }
   public static void main(String[] args) {
-    System.out.println(add(add(10, 20), add(30, 40)));
+    System.out.println(add(10, 20)); //1つ目のaddメソッドが呼び出される
+    System.out.println(add(3.5, 2.7));  //2つ目のaddメソッドが呼び出される
+    System.out.println(add("Hello", "World"));  //3つ目のaddメソッドが呼び出される
   }
-  //※return文は値を戻すだけでなく、メソッドの終了も行います。そのため、return文の後に処理を書いても実行されない
 }
