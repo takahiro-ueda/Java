4b03979eea1821219c65c5f3f7c7f5e6a21ac4ea
takahiro-ueda <pt25ue@gmail.com>
2020-07-29 16:21:57 +0900

    オーバーロード(引数の数が異なる場合)

--- JavaLearn/src/lesson5.java
+++ JavaLearn/src/lesson5.java
@@ -1,20 +1,15 @@
 public class lesson5 {
   //1つ目のaddメソッド
-  public static int add(int x, int y) {
+  public static int add(int x, int y) { //1つ目のaddメソッド
     return x + y;
   }
   //2つ目のaddメソッド
-  public static double add(double x, double y) {
-    return x + y;
-  }
-  //3つ目のaddメソッド
-  public static String add(String x, String y) {
-    return x + y;
+  public static int add(int x, int y, int z) {  //2つ目のaddメソッド
+    return x + y + z;
   }
   public static void main(String[] args) {
-    System.out.println(add(10, 20)); //1つ目のaddメソッドが呼び出される
-    System.out.println(add(3.5, 2.7));  //2つ目のaddメソッドが呼び出される
-    System.out.println(add("Hello", "World"));  //3つ目のaddメソッドが呼び出される
+    System.out.println("10+20= " + add(10, 20)); //1つ目のaddメソッドが呼び出される
+    System.out.println("10+20+30= " + add(10, 20, 30));  //2つ目のaddメソッドが呼び出される
   }
-  //仮引数が異なれば同じ名前のメリットを複数定義することが許されています
+  //仮引数の型だけでなく、個数が違う場合もオーバーロードできます
 }
