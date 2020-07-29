772f3043c6d8d62e40cc3f6400a3665dfcc72d62
takahiro-ueda <pt25ue@gmail.com>
2020-07-29 16:56:22 +0900

    戻り値が配列の場合

--- JavaLearn/src/lesson5.java
+++ JavaLearn/src/lesson5.java
@@ -1,14 +1,30 @@
 public class lesson5 {
   //int型配列を受け取り、配列内の要素全てに１を加えるメソッド
-  public static void incArray(int[] args) {
-    for (int i = 0; i < array.length; i++) {
-      array[i]++;
+  // public static void incArray(int[] args) {
+  //   for (int i = 0; i < array.length; i++) {
+  //     array[i]++;
+  //   }
+  // }
+  // //↑結果をreturnで返していないためエラー
+  // public static void main(String[] args) {
+  //   int[] array = {1, 2, 3};
+  //   incArray(array);  //メソッド実行
+  //   for (int i : array) {
+  //     System.out.println(i);  //arrayの全要素を出力
+  //   }
+  // }
+
+  //            ↓　変更
+
+  public static int[] makeArray(int size) { //int型配列を作成して戻すメソッド
+    int[] newArray = new int[size];
+    for (int i = 0; i < newArray.length; i++) {
+      newArray[i] = i;
     }
+    return newArray;  //配列を戻す
   }
-  //↑結果をreturnで返していないためエラー
   public static void main(String[] args) {
-    int[] array = {1, 2, 3};
-    incArray(array);  //メソッド実行
+    int[] array = makeArray(3);
     for (int i : array) {
       System.out.println(i);  //arrayの全要素を出力
     }
