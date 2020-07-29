975c4d0a7c7b4c562e93ff1d741a831529f7476b
takahiro-ueda <pt25ue@gmail.com>
2020-07-29 11:59:22 +0900

    引数の例(渡す値が1つの場合)

--- JavaLearn/src/lesson5.java
+++ JavaLearn/src/lesson5.java
@@ -1,22 +1,12 @@
 public class lesson5 {
-  /*
   public static void main(String[] args) {
     System.out.println("メソッドを呼び出します");
-    hello();  //helloメソッドを呼び出す
+    hello("植田");  //"植田"を渡してhelloメソッドを呼び出す
+    hello("朝香");  //"朝香"を渡してhelloメソッドを呼び出す
+    hello("菅原");  //"菅原"を渡してhelloメソッドを呼び出す
     System.out.println("メソッドの呼び出しが終わりました");
   }
-  public static void hello() {
-    System.out.println("湊さん、こんにちは！！！！");
-  }
-  */
-  public static void methodA() {
-    System.out.println("methodA");
-    methodB();  //methodB メソッドの呼び出し
-  }
-  public static void methodB() {
-    System.out.println("methodB");
-  }
-  public static void main(String[] args) {
-    methodA();  //methodA メソッドの呼び出し
+  public static void hello(String name) {
+    System.out.println(name + "さん、こんにちは");
   }
 }
