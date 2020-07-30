790288057421796a2f62c0788b507509771bec0f
takahiro-ueda <pt25ue@gmail.com>
2020-07-30 15:31:53 +0900

    正しく修正したlesson6.java

--- JavaLearn/src/lesson6.java
+++ JavaLearn/src/lesson6.java
@@ -1,10 +1,9 @@
 public class lesson6 {
   public static void main(String[] args) {
     int a = 10; int b = 2;
-    int total = tasu(a, b);
-    int delta = hiku(a, b);
+    int total = CalcLogic.tasu(a, b);
+    int delta = CalcLogic.hiku(a, b);
+    //  ↑ CalcLogicのtasu()やhiku()のように明示的に所属を示す必要がある
     System.out.println("足すと" + total +"、 引くと" + delta);
   }
-  //このままでは[tasu()やhiku()メソッドがないので呼び出すことができない]
-  //lesson6.javaにはtasu()やhiku()は存在しない
 }
