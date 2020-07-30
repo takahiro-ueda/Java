e74d9c883bfb53991224915caa52bfc351026eb8
takahiro-ueda <pt25ue@gmail.com>
2020-07-30 15:01:48 +0900

    練習問題

--- JavaLearn/src/lesson5.java
+++ JavaLearn/src/lesson5.java
@@ -1,32 +1,63 @@
 public class lesson5 {
-  //int型配列を受け取り、配列内の要素全てに１を加えるメソッド
-  // public static void incArray(int[] args) {
-  //   for (int i = 0; i < array.length; i++) {
-  //     array[i]++;
-  //   }
-  // }
-  // //↑結果をreturnで返していないためエラー
+  //練習問題5-1
   // public static void main(String[] args) {
-  //   int[] array = {1, 2, 3};
-  //   incArray(array);  //メソッド実行
-  //   for (int i : array) {
-  //     System.out.println(i);  //arrayの全要素を出力
-  //   }
+  //   introduceOneself();
   // }
 
-  //            ↓　変更
+  // public static void introduceOneself() {
+  //   String name = "Minato";
+  //   int age = 22;
+  //   double height = 169.9;
+  //   char zodiac = '鼠';
+  //   System.out.println("私の名前は" + name + "です");
+  //   System.out.println("歳は" + age + "です");
+  //   System.out.println("身長は" + height + "cmです");
+  //   System.out.println("十二氏は" + zodiac + "です");
+  // }
 
-  public static int[] makeArray(int size) { //int型配列を作成して戻すメソッド
-    int[] newArray = new int[size];
-    for (int i = 0; i < newArray.length; i++) {
-      newArray[i] = i;
-    }
-    return newArray;  //配列を戻す
-  }
+  //練習問題5-2
+  // public static void main(String[] args) {
+  //   String title = "お誘い";
+  //   String address = "uso800@xxx.com";
+  //   String text = "今度、飲みに行きませんか";
+  //   email(title, address, text);
+  // }
+  // public static void email(String title, String address, String text) {
+  //   System.out.println(address + "に、以下のメールを送信しました");
+  //   System.out.println("件名: " + title);
+  //   System.out.println("本文: " + text);
+  // }
+
+  //練習問題5-3
+  // public static void main(String[] args) {
+  //   String address = "uso800@xxx.com";
+  //   String text = "今度、飲みに行きませんか";
+  //   email(address, text);
+  // }
+  // public static void email(String address, String text) {
+  //   System.out.println(address + "に、以下のメールを送信しました");
+  //   System.out.println("件名: 無題");
+  //   System.out.println("本文: " + text);
+  // }
+  // public static void email(String title, String address, String text) {
+  //   System.out.println(address + "に、以下のメールを送信しました");
+  //   System.out.println("件名: " + title);
+  //   System.out.println("本文: " + text);
+  // }
+
+  //練習問題5-4
   public static void main(String[] args) {
-    int[] array = makeArray(3);
-    for (int i : array) {
-      System.out.println(i);  //arrayの全要素を出力
-    }
+    double triangleArea = calcTriangleArea(10.0, 5.0);
+    System.out.println("三角形の面積: " + triangleArea + "平方cm");
+    double circleArea = calcCircleArea(5.0);
+    System.out.println("円の面積: " + circleArea + "平方cm");
+  }
+  public static double calcTriangleArea(double bottom, double height ) {
+    double area = (bottom * height) / 2;
+    return area;
+  }
+  public static double calcCircleArea(double radius) {
+    double area = radius * radius * 3.14;
+    return area;
   }
 }
