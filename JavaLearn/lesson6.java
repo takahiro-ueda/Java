a8ac1986b29ab452159fafef731b18d483710a88
takahiro-ueda <pt25ue@gmail.com>
2020-07-30 16:22:46 +0900

    別のパッケージにあるクラスを呼び出す

--- JavaLearn/lesson6.java
+++ JavaLearn/lesson6.java
@@ -1,9 +1,11 @@
 package calcapp.main;
+
+
 public class lesson6 {
   public static void main(String[] args) {
     int a = 10; int b = 2;
-    int total = CalcLogic.tasu(a, b);
-    int delta = CalcLogic.hiku(a, b);
+    int total = calcapp.logics.CalcLogic.tasu(a, b);
+    int delta = calcapp.logics.CalcLogic.hiku(a, b);
     //  ↑ CalcLogicのtasu()やhiku()のように明示的に所属を示す必要がある
     System.out.println("足すと" + total +"、 引くと" + delta);
   }
