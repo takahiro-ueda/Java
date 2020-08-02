a9192e23e7779a5f78fa3b8c628dc9afad938208
takahiro-ueda <pt25ue@gmail.com>
2020-08-03 00:03:13 +0900

    中身のない空のHeroクラスを作成

--- JavaLearn/src/Hero.java
+++ JavaLearn/src/Hero.java
@@ -1,12 +1,12 @@
 public class Hero {
-  //属性の定義
-  String name;
-  int hp;
+  // //属性の定義
+  // String name;
+  // int hp;
 
-  //操作の定義
-  public void attack() {}
-  public void sleep() {}
-  public void sit(int sec) {}
-  public void slip() {}
-  public void run () {}
+  // //操作の定義
+  // public void attack() {}
+  // public void sleep() {}
+  // public void sit(int sec) {}
+  // public void slip() {}
+  // public void run () {}
 }
