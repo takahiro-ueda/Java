983e04a1403703d206c44ff73af8f0456b72a04f
takahiro-ueda <pt25ue@gmail.com>
2020-08-03 00:17:01 +0900

    「眠る」操作に含まれる要素を記述

--- JavaLearn/src/Hero.java
+++ JavaLearn/src/Hero.java
@@ -3,9 +3,12 @@ public class Hero {
   String name;  //名前の宣言
   int hp; //HPの宣言
 
-  // //操作の定義
+  //操作の定義
   // public void attack() {}
-  // public void sleep() {}
+  public void sleep() {
+    this.hp = 100;
+    System.out.println(this.name + "は、眠って回復した！");
+  }
   // public void sit(int sec) {}
   // public void slip() {}
   // public void run () {}
