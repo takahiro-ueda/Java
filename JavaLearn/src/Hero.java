c5cecadd1d3f512c8beaff01bbaf395236aa0569
takahiro-ueda <pt25ue@gmail.com>
2020-08-03 21:22:10 +0900

    メソッド「座る」「転ぶ」「逃げる」を追加

--- JavaLearn/src/Hero.java
+++ JavaLearn/src/Hero.java
@@ -5,11 +5,23 @@ public class Hero {
 
   //操作の定義
   // public void attack() {}
-  public void sleep() {
+  public void sleep() { //眠るメソッド
     this.hp = 100;
     System.out.println(this.name + "は、眠って回復した！");
   }
-  // public void sit(int sec) {}
-  // public void slip() {}
-  // public void run () {}
+  public void sit(int sec) {  //座るメソッド
+    this.hp += sec;
+    System.out.println(this.name + "は、" + sec + "秒座った！！");
+    System.out.println("HPが" + sec + "ポイント回復した");
+  }
+  public void slip() {  //転ぶメソッド
+    this.hp = 5;
+    System.out.println(this.name + "は、転んだ！！");
+    System.out.println("5のダメージ");
+  }
+  public void run () {  //逃げるメソッド
+    System.out.println(this.name + "は、逃げ出した");
+    System.out.println("GAMEOVER");
+    System.out.println("最終HPは" + this.hp + "でした！！");
+  }
 }
