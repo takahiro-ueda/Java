537710f9b86dab20b6aa6c87bb8c2686d8ec6969
takahiro-ueda <pt25ue@gmail.com>
2020-08-03 22:51:35 +0900

    2つのHero型変数を利用する

--- JavaLearn/lesson9.java
+++ JavaLearn/lesson9.java
@@ -1,7 +1,11 @@
 public class lesson9 {
   public static void main(String[] args) {
-    Hero h;
-    h = new Hero();
+    Hero h1;
+    h1 = new Hero();
     h.hp = 100;
+    Hero h2;
+    h2 = h1;
+    h2.hp = 200;
+    System.out.println(h1.hp);
   }
 }
