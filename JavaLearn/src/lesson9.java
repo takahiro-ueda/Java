81bc053df342f246d5b04664306536b1d4bf1ba1
takahiro-ueda <pt25ue@gmail.com>
2020-08-03 23:11:15 +0900

    剣を装備した勇者を生み出す

--- JavaLearn/src/lesson9.java
+++ JavaLearn/src/lesson9.java
@@ -1,11 +1,12 @@
 public class lesson9 {
   public static void main(String[] args) {
-    Hero h1;
-    h1 = new Hero();
-    h1.hp = 100;
-    Hero h2;
-    h2 = h1;
-    h2.hp = 200;
-    System.out.println(h1.hp);
+    Sword s = new Sword();
+    s.name = "炎の剣";
+    s.damage = 10;
+    Hero h = new Hero();
+    h.name = "ミナト";
+    h.hp = 100;
+    h.sword = s;  //swordフィールドに生成済みのインスタンス(の番地)を代入
+    System.out.println("現在の武器は" + h.sword.name);  //勇者「の」剣「の」
   }
 }
