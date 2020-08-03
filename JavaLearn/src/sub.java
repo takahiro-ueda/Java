f1177f98ed4c4b4605de97f81fa96b340a3ebb23
takahiro-ueda <pt25ue@gmail.com>
2020-08-03 22:17:44 +0900

    仮想世界に勇者とお化けキノコ２匹を生み出す

--- JavaLearn/src/sub.java
+++ JavaLearn/src/sub.java
@@ -2,15 +2,26 @@ public class sub {
   public static void main(String[] args) {
     //  1.勇者を生成
     Hero h = new Hero(); //Heroクラスからインスタンスを生成し、変数hにいれる
+
     //  2.フィールドに初期値をセット
     h.name = "湊";
     h.hp = 100;
     System.out.println("勇者" + h.name + "を生み出しました！");
-    //  3.勇者のメソッドを呼び出してゆく
-    h.sit(5); //5秒座れ
-    h.slip(); //転ぶ
-    h.sit(25);  //25座れ
-    h.run();  //逃げろ
+
+    Matango m1 = new Matango(); //お化けキノコA(１匹目)を生成し初期化
+    m1.hp = 50;
+    m1.suffix = 'A';
+
+    Matango m2 = new Matango(); //お化けキノコ(2匹目)を生成し初期化
+    m2.hp = 48;
+    m2.suffix = 'B';
+
+    //  冒険の始まり
+    h.slip(); //勇者は転ぶ
+    m1.run(); //お化けきのこAが逃げる
+    m2.run(); //お化けキノコBが逃げる
+    h.run();  //勇者も逃げろ
+
     // int yusha_hp = 100;
     // int matango1_hp = 50;
     // int matango2_hp = 48;
