9fc70717a6c157a6fb29f1825834865331bed383
takahiro-ueda <pt25ue@gmail.com>
2020-08-03 21:31:41 +0900

    勇者インスタンスを生成して初期値を代入

--- JavaLearn/src/sub.java
+++ JavaLearn/src/sub.java
@@ -2,5 +2,9 @@ public class sub {
   public static void main(String[] args) {
     //  1.勇者を生成
     Hero h = new Hero(); //Heroクラスからインスタンスを生成し、変数hにいれる
+    //  2.フィールドに初期値をセット
+    h.name = "湊";
+    h.hp = 100;
+    System.out.println("勇者" + h.name + "を生み出しました！");
   }
 }
