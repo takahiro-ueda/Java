ae9a9a6218dabf23e0b636dfab0768838798610c
takahiro-ueda <pt25ue@gmail.com>
2020-08-03 21:25:29 +0900

    勇者インスタンスを生成

--- JavaLearn/src/sub.java
+++ JavaLearn/src/sub.java
@@ -1,9 +1,6 @@
 public class sub {
   public static void main(String[] args) {
-    //(以下の内容をJavaで記述していく)
-    //勇者よ、この仮想世界に生まれよ！
-    //お化けキノコよ、この仮想世界に生まれよ！
-    //勇者よ、戦え！
-    //お化けキノコよ、逃げろ！
+    //  1.勇者を生成
+    Hero h = new Hero(); //Heroクラスからインスタンスを生成し、変数hにいれる
   }
 }
