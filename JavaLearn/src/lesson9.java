b5631df33a772f6a486135538fcd35214ce424f1
takahiro-ueda <pt25ue@gmail.com>
2020-08-04 11:42:43 +0900

    コンストラクタをオーバーロードしたクラスの利用

--- JavaLearn/src/lesson9.java
+++ JavaLearn/src/lesson9.java
@@ -12,8 +12,10 @@ public class lesson9 {
     // w.heal(h1); //ミナトを回復させる(HP: 100 → 110)
     // w.heal(h2); //アサカを回復させる(HP: 100 → 110)
     // w.heal(h2); //アサカを回復させる(HP: 110 → 120)
-    Hero h = new Hero();  //インスタンス生成と同時にコンストラクタによってHPに１００が代入される
-    System.out.println(h.hp); //「１００」と表示させる
-    System.out.println(h.name); //「ミナト」と表示される
+
+    Hero h1 = new Hero("ミナト");  //インスタンス生成と同時にコンストラクタによってHPに１００が代入される
+    System.out.println(h1.name); //「ミナト」と表示される
+    Hero h2 = new Hero(); //引数がないのでコンストラクタ2が呼び出される
+    System.out.println(h2.name);
   }
 }
