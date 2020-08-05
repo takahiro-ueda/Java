03a79c841cba204ebbc379ea659cbeaeb82afb56
takahiro-ueda <pt25ue@gmail.com>
2020-08-05 16:00:50 +0900

    Characterクラスを作成(未完成)

---
+++ JavaLearn/src/lesson11/Character.java
@@ -0,0 +1,18 @@
+package lesson11;
+
+public class Character {
+  String name;
+  int hp;
+
+  //逃げる
+  public void run() {
+    System.out.println(this.name + "は逃げ出した！！！");
+  }
+
+  //戦う
+  public void attack(Matango m) {
+    System.out.println(this.name + "の攻撃！");
+    m.hp -= ??; //ここを記述しようとして手が止まる
+    System.out.println("敵に？？ポイントのダメージをあたえた！");
+  }
+}
