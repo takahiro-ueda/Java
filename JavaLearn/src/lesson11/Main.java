62a2bf505fd4357eb580dee482b834a3851ddfa1
takahiro-ueda <pt25ue@gmail.com>
2020-08-05 22:24:52 +0900

    継承の材料をnewして利用されてしまう

--- JavaLearn/src/lesson11/Main.java
+++ JavaLearn/src/lesson11/Main.java
@@ -2,8 +2,8 @@ package lesson11;
 
 public class Main {
   public static void main(String[] args) {
-    Hero h = new Hero();
-    Matango m = new Matango();
-    h.attack(m);
+    Character c = new Character();  //HeroやWizardではなくCharacterをnewしてしまった！
+    Matango m = new Matango('A');
+    c.attack(m);  //中身がないので何も動かない！
   }
 }
