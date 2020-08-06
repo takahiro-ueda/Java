245410545c19d1ef6349c5a81b50951d63725933
takahiro-ueda <pt25ue@gmail.com>
2020-08-06 22:56:08 +0900

    王様クラスで利用されるnameフィールド

--- JavaLearn/src/lesson13/King.java
+++ JavaLearn/src/lesson13/King.java
@@ -1,7 +1,7 @@
 package lesson13;
 
 public class King {
-  public void talk(Hero h) {
+  void talk(Hero h) {
     System.out.println("王様：ようこそ我が国へ、勇者" + h.name + "よ。");
     System.out.println("王様：長旅疲れたであろう。");
     System.out.println("王様：まずは城下町を見てくると良い。ではまた会おう。");
