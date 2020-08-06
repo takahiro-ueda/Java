d5acfa75e775f9edcb1950ded25a6ebcd107f881
takahiro-ueda <pt25ue@gmail.com>
2020-08-06 10:03:46 +0900

    Wizardクラスを通常の方法でインスタンス化

--- JavaLearn/src/lesson12/Main.java
+++ JavaLearn/src/lesson12/Main.java
@@ -2,6 +2,10 @@ package lesson12;
 
 public class Main {
   public static void main(String[] args) {
-    Life lf = new Wizard(); //Wizardは生き物の一種
+    Wizard w = new Wizard(); //Wizardは生き物の一種
+    Matango m = new Matango();
+    w.name = "アサカ";
+    w.attack(m);
+    w.fireball(m);
   }
 }
