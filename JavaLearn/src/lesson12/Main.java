0b335ab339b53a386253de0518b517377414b649
takahiro-ueda <pt25ue@gmail.com>
2020-08-06 22:11:11 +0900

    同じ指示で異なる動作をさせることができる

--- JavaLearn/src/lesson12/Main.java
+++ JavaLearn/src/lesson12/Main.java
@@ -12,17 +12,15 @@ public class Main {
     // Slime s = new Slime(); Monster m = new Slime();
     // s.run(); m.run();
 
-    Character[] c = new Character[5];
-    c[0] = new Hero();
-    c[1] = new Hero();
-    c[2] = new Hero();
-    c[3] = new Hero();
-    c[4] = new Hero();
+    Monster[] monsters = new Monster[3];
+    monsters[0] = new Slime();
+    monsters[1] = new Goblin();
+    monsters[2] = new DeathBat();
 
     //冒険開始！！
     //まず宿屋に泊まる
-    for (Character ch : c) {  //1名ずつ順に取り出し
-      ch.hp += 50;  //HPを50回復する
+    for (Monster m : monsters) {
+      m.run();  //同じ指示を繰り返す
     }
   }
 }
