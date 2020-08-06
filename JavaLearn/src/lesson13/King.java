34f27cd00a0d377fd8e5fcf83e7d696ceb0b6f38
takahiro-ueda <pt25ue@gmail.com>
2020-08-06 23:04:57 +0900

    talk()でgetName()を呼び出す

--- JavaLearn/src/lesson13/King.java
+++ JavaLearn/src/lesson13/King.java
@@ -2,7 +2,7 @@ package lesson13;
 
 public class King {
   void talk(Hero h) {
-    System.out.println("王様：ようこそ我が国へ、勇者" + h.name + "よ。");
+    System.out.println("王様：ようこそ我が国へ、勇者" + h.getName() + "よ。");
     System.out.println("王様：長旅疲れたであろう。");
     System.out.println("王様：まずは城下町を見てくると良い。ではまた会おう。");
     h.die();  //ここが不具合の原因。勇者が死ぬ！
