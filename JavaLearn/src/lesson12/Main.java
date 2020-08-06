578b8eb4c0ab2b37d0d190582ddef63e7290d8ec
takahiro-ueda <pt25ue@gmail.com>
2020-08-06 16:52:57 +0900

    パーティが宿屋に泊まってHPを回復する

--- JavaLearn/src/lesson12/Main.java
+++ JavaLearn/src/lesson12/Main.java
@@ -9,7 +9,25 @@ public class Main {
     // w.attack(m);
     // w.fireball(m);
 
-    Slime s = new Slime(); Monster m = new Slime();
-    s.run(); m.run();
+    // Slime s = new Slime(); Monster m = new Slime();
+    // s.run(); m.run();
+
+    Hero h1 = new Hero();
+    Hero h2 = new Hero();
+    Thief t1 = new Thief();
+    Wizard w1 = new Wizard();
+    Wizard w2 = new Wizard();
+    //冒険開始！！
+    //まず宿屋に泊まる
+    h1.hp += 50;
+    h2.hp += 50;
+    t1.hp += 50;
+    w1.hp += 50;
+    w2.hp += 50;
+    /* 
+    ※このコードの前提
+    ・HeroやWizard、Thiefは、抽象クラスCharacterを継承している。
+    ・Characterはnameとhpフィールド、attack()とrun()のメソッドを持つ。
+    */
   }
 }
