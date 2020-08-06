6fe11cf798ad660844466760fb579c964cbf5500
takahiro-ueda <pt25ue@gmail.com>
2020-08-06 22:47:48 +0900

    dieメソッドをprivateとして指定する

--- JavaLearn/src/lesson13/Hero.java
+++ JavaLearn/src/lesson13/Hero.java
@@ -8,7 +8,7 @@ public class Hero {
   public void bye() {
     System.out.println("勇者は別れを告げた");
   }
-  public void die() {
+  private void die() {
     System.out.println(this.name + "は死んでしまった！");
     System.out.println("GAME OVERです！！");
   }
