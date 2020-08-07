a0789c2dbcd5eb80304cae9ec4648428ff992765
takahiro-ueda <pt25ue@gmail.com>
2020-08-07 15:00:12 +0900

    どんなインスタンスでも受け取れるメソッド

--- JavaLearn/src/lesson14/Main.java
+++ JavaLearn/src/lesson14/Main.java
@@ -1,12 +1,16 @@
 package lesson14;
 
 public class Main {
-  public static void main(String[] args) {
-    // Empty e = new Empty();
-    // String s = e.toString();
-    // System.out.println(s);
-    Object o1 = new Empty();
-    Object o2 = new Hero();
-    Object o3 = new "こんにちは";
+  // public static void main(String[] args) {
+  //   // Empty e = new Empty();
+  //   // String s = e.toString();
+  //   // System.out.println(s);
+  //   Object o1 = new Empty();
+  //   Object o2 = new Hero();
+  //   Object o3 = new "こんにちは";
+  // }
+  public void printAnything(Object o) {
+    //何型でもいいから、引数を1つ受け取り画面に表示
+    System.out.println(o.toString());
   }
 }
