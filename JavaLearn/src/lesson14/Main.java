b8b0ed5ba4626fec50432bf5296786ad5396295e
takahiro-ueda <pt25ue@gmail.com>
2020-08-07 14:56:15 +0900

    Object型にさまざまなインスタンスを代入

--- JavaLearn/src/lesson14/Main.java
+++ JavaLearn/src/lesson14/Main.java
@@ -2,8 +2,11 @@ package lesson14;
 
 public class Main {
   public static void main(String[] args) {
-    Empty e = new Empty();
-    String s = e.toString();
-    System.out.println(s);
+    // Empty e = new Empty();
+    // String s = e.toString();
+    // System.out.println(s);
+    Object o1 = new Empty();
+    Object o2 = new Hero();
+    Object o3 = new "こんにちは";
   }
 }
