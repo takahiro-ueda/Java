670c3b1b6769d4c1507861e6924aa2c823960dbd
takahiro-ueda <pt25ue@gmail.com>
2020-08-07 15:14:29 +0900

    意図する文字列表現を返すtoString()を定義

--- JavaLearn/src/lesson14/Hero.java
+++ JavaLearn/src/lesson14/Hero.java
@@ -1,5 +1,32 @@
 package lesson14;
 
 public class Hero {
-  
+  int hp;
+  String name;
+  Sword sword;
+
+  public void bye() {
+    System.out.println("勇者は別れを告げた");
+  }
+  private void die() {
+    System.out.println(this.name + "は死んでしまった！");
+    System.out.println("GAME OVERです！！");
+  }
+  public void sleep() {
+    this.hp = 100;
+    System.out.println(this.name + "は眠って回復した！");
+  }
+  public void attack(Matango m) {
+    System.out.println(this.name + "の攻撃！");
+    System.out.println("敵に10ポイントのダメージをあたえた！！！");
+    m.hp -= 10;
+    System.out.println("お化けキノコ" + m.suffix + "から2ポイントの反撃を受けた");
+    this.hp -= 2;
+    if (this.hp <= 0 ) {
+      this.die();
+    }
+  }
+  public String toString() {  //オーバーライドする
+    return "名前：" + this.name + "/HP：" + this.hp;
+  }
 }
