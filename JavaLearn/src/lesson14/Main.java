2b0e3aa8023d87260bbd6acd7a14bb4ae901f5f6
takahiro-ueda <pt25ue@gmail.com>
2020-08-07 15:19:25 +0900

    2人の勇者を比較する

--- JavaLearn/src/lesson14/Main.java
+++ JavaLearn/src/lesson14/Main.java
@@ -10,10 +10,17 @@ public class Main {
     // Object o2 = new Hero();
     // Object o3 = new "こんにちは";
 
-    Hero h = new Hero();
-    h.name = "ミナト";
-    h.hp = 100;
-    System.out.println(h.toString());
+    Hero h1 = new Hero(); //1人目の勇者
+    h1.name = "ミナト";
+    h1.hp = 100;
+    Hero h2 = new Hero(); //2人目の勇者
+    h2.name = "ミナト";
+    h2.hp = 100;
+    if (h1.equals(h2) == true) {
+      System.out.println("同じ内容です！");
+    } else {
+      System.out.println("違う内容です！！");
+    }
   }
   // public void printAnything(Object o) {
   //   //何型でもいいから、引数を1つ受け取り画面に表示
