9475d0bf0f87b820d3c93b6cb811590bdb610803
takahiro-ueda <pt25ue@gmail.com>
2020-08-09 07:30:40 +0900

    Setに重複した値を追加しようとすると・・・・

--- JavaLearn/src/lesson16/Main.java
+++ JavaLearn/src/lesson16/Main.java
@@ -1,16 +1,14 @@
 package lesson16;
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
   public static void main(String[] args) {
-    ArrayList<String> names = new ArrayList<String>();
-    names.add("湊");
-    names.add("朝香");
-    names.add("菅原");
-    Iterator<String> it = names.iterator();
-    while (it.hasNext()) {
-      String e = it.next();
-      System.out.println(e);
-    }
+    Set<String> colors = new HashSet<String>();
+    colors.add("赤");
+    colors.add("青");
+    colors.add("黄");
+    colors.add("赤"); //重複して「赤」を格納しようとしても無視される
+    System.out.println("色は" + colors.size() + "種類");
   }
 }
