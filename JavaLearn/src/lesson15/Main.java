aad269785b2f03d9553d833726d16d2c02298ab4
takahiro-ueda <pt25ue@gmail.com>
2020-08-08 21:40:45 +0900

    Periodクラスの利用例

--- JavaLearn/src/lesson15/Main.java
+++ JavaLearn/src/lesson15/Main.java
@@ -1,22 +1,16 @@
 package lesson15;
 import java.time.*;
-import java.time.format.*;
 
 public class Main {
   public static void main(String[] args) {
-    //文字列からLocalDateを作成
-    DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-    LocalDate ldate = LocalDate.parse("2020/09/22", fmt);
+    LocalDate d1 = LocalDate.of(2020,1,1);
+    LocalDate d2 = LocalDate.of(2020,1,4);
 
-    //1000日後を計算する
-    LocalDate ldatep = ldate.plusDays(1000);
-    String str = ldatep.format(fmt);
-    System.out.println("1000日後は" + str);
+    //3日間を表すPeriodを2通りの方法で生成
+    Period p1 = Period.ofDays(3);
+    Period p2 = Period.between(d1, d2);
 
-    //現在日付との比較
-    LocalDate now = LocalDate.now();
-    if (now.isAfter(ldatep)) {
-      System.out.println("1000日後は過去日付です");
-    }
+    //d2のさらに3日後を計算する
+    LocalDate d3 = d2.plus(p2);
   }
 }
