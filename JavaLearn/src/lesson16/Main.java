53e2af2293a048e616a04b443803dd7e8b18eead
takahiro-ueda <pt25ue@gmail.com>
2020-08-09 07:57:59 +0900

    HashMapクラスの利用

--- JavaLearn/src/lesson16/Main.java
+++ JavaLearn/src/lesson16/Main.java
@@ -1,17 +1,18 @@
 package lesson16;
-import java.util.Set;
-import java.util. TreeSet;
+import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
-    Set<String> words = new TreeSet<String>();
-    //赤・青・黄の順に格納
-    words.add("dog");
-    words.add("cat");
-    words.add("wolf");
-    words.add("panda");
-    for(String s : words) {
-      System.out.println(s + "→");
-    }
+    Map<String, Integer> prefs = new HashMap<String, Integer>();
+     //ペアで値を格納
+    prefs.put("京都府", 255);
+    prefs.put("東京都", 1261);
+    prefs.put("熊本県", 181);
+    int tokyo = prefs.get("東京都"); //キーを指定して値を取得
+    System.out.println("東京都の人口は" + tokyo);
+    prefs.remove("京都府");
+    prefs.put("熊本県", 182); //値を182で上書き
+    int kumamoto = prefs.get("熊本県");
+    System.out.println("熊本県の人口は、" + kumamoto);
   }
 }
