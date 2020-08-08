f6123c7f4603a09d840dfd50d6d498df6d644d7c
takahiro-ueda <pt25ue@gmail.com>
2020-08-08 11:00:09 +0900

    各インスタンスから静的フィールドを利用可能

--- JavaLearn/src/lesson14/Main.java
+++ JavaLearn/src/lesson14/Main.java
@@ -12,8 +12,11 @@ public class Main {
 
     Hero h1 = new Hero(); //1人目の勇者
     Hero h2 = new Hero(); //2人目の勇者
-    System.out.println(h1.hp);  //インスタンスh1の箱hpを表示
+    Hero.money = 100;
     System.out.println(Hero.money); //クラスHeroの箱moneyを表示
+    System.out.println(h1.money);  //インスタンスh1の箱hpを表示
+    h1.money = 300;
+    System.out.println(h2.money);
   }
   // public void printAnything(Object o) {
   //   //何型でもいいから、引数を1つ受け取り画面に表示
