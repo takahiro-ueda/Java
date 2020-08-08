6bbb6d4bde8d9098e8050c0c857b3930785461ba
takahiro-ueda <pt25ue@gmail.com>
2020-08-08 11:16:13 +0900

    newせずとも静的メソッドは呼び出し可能

--- JavaLearn/src/lesson14/Main.java
+++ JavaLearn/src/lesson14/Main.java
@@ -2,9 +2,14 @@ package lesson14;
 
 public class Main {
   public static void main(String[] args) {
+    Hero.setRandomMoney();
+    System.out.println(Hero.money); //ランダムな金額を表示
+    Hero h1 = new Hero();
+    System.out.println(h1.money); //同じ額を表示
+
     //1人も勇者を生み出していない状況で・・・・
-    Hero.money = 100;
-    System.out.println(Hero.money);
+    // Hero.money = 100;
+    // System.out.println(Hero.money);
 
     // Hero h1 = new Hero(); //1人目の勇者
     // Hero h2 = new Hero(); //2人目の勇者
