95e2779eccb06d8bfb393f43010386864fb7c1f5
takahiro-ueda <pt25ue@gmail.com>
2020-08-08 11:05:35 +0900

    newせずとも静的フィールドは利用可能

--- JavaLearn/src/lesson14/Main.java
+++ JavaLearn/src/lesson14/Main.java
@@ -2,24 +2,16 @@ package lesson14;
 
 public class Main {
   public static void main(String[] args) {
-    // Empty e = new Empty();
-    // String s = e.toString();
-    // System.out.println(s);
-
-    // Object o1 = new Empty();
-    // Object o2 = new Hero();
-    // Object o3 = new "こんにちは";
-
-    Hero h1 = new Hero(); //1人目の勇者
-    Hero h2 = new Hero(); //2人目の勇者
+    //1人も勇者を生み出していない状況で・・・・
     Hero.money = 100;
-    System.out.println(Hero.money); //クラスHeroの箱moneyを表示
-    System.out.println(h1.money);  //インスタンスh1の箱hpを表示
-    h1.money = 300;
-    System.out.println(h2.money);
+    System.out.println(Hero.money);
+
+    // Hero h1 = new Hero(); //1人目の勇者
+    // Hero h2 = new Hero(); //2人目の勇者
+    // Hero.money = 100;
+    // System.out.println(Hero.money); //クラスHeroの箱moneyを表示
+    // System.out.println(h1.money);  //インスタンスh1の箱hpを表示
+    // h1.money = 300;
+    // System.out.println(h2.money);
   }
-  // public void printAnything(Object o) {
-  //   //何型でもいいから、引数を1つ受け取り画面に表示
-  //   System.out.println(o.toString());
-  // }
 }
