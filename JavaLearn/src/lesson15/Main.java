3476c15349885c9bea7f589be13195a96a16b6e6
takahiro-ueda <pt25ue@gmail.com>
2020-08-08 15:35:39 +0900

    文字列切り出しメソッドを利用した例

--- JavaLearn/src/lesson15/Main.java
+++ JavaLearn/src/lesson15/Main.java
@@ -2,15 +2,20 @@ package lesson15;
 
 public class Main {
   public static void main(String[] args) {
-    String s1 = "Java and JavaScript";
-    if (s1.contains("Java")) {
-      System.out.println("文字列s1は、Javaを含んでいます");
-    }
-    if (s1.endsWith("Java")) {
-      System.out.println("文字列s1は、Javaが末尾にあります");
-    }
-    System.out.println("文字列s1で最初にJavaが登場する位置は" + s1.indexOf("Java"));
-    System.out.println("文字列s1で最後にJavaが登場する位置は" + s1.lastIndexOf("Java"));
+    String s1 = "Java programming";
+    System.out.println("文字列s1の4文字目以降は" + s1.substring(3));  // => a programming
+    System.out.println("文字列s1の4~8文字目は" + s1.substring(3, 8)); // => a pro
+    
+    // String s1 = "Java and JavaScript";
+    // if (s1.contains("Java")) {
+    //   System.out.println("文字列s1は、Javaを含んでいます");
+    // }
+    // if (s1.endsWith("Java")) {
+    //   System.out.println("文字列s1は、Javaが末尾にあります");
+    // }
+    // System.out.println("文字列s1で最初にJavaが登場する位置は" + s1.indexOf("Java"));
+    // System.out.println("文字列s1で最後にJavaが登場する位置は" + s1.lastIndexOf("Java"));
+
     // String s2 = "Java";
     // String s3 = "Java";
     // if (s2.equals(s3)) {
