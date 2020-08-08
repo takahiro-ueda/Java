6572c7a747538d16d9eda955aeb6b03b84784012
takahiro-ueda <pt25ue@gmail.com>
2020-08-09 07:37:22 +0900

    TreeSetから文字列を取り出すと・・・・

--- JavaLearn/src/lesson16/Main.java
+++ JavaLearn/src/lesson16/Main.java
@@ -1,15 +1,16 @@
 package lesson16;
-import java.util.HashSet;
 import java.util.Set;
+import java.util. TreeSet;
 
 public class Main {
   public static void main(String[] args) {
-    Set<String> colors = new HashSet<String>();
+    Set<String> words = new TreeSet<String>();
     //赤・青・黄の順に格納
-    colors.add("赤");
-    colors.add("青");
-    colors.add("黄");
-    for(String s : colors) {
+    words.add("dog");
+    words.add("cat");
+    words.add("wolf");
+    words.add("panda");
+    for(String s : words) {
       System.out.println(s + "→");
     }
   }
