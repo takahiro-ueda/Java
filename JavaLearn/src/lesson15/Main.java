e0e3ac568cb772b37f3b0df854e1f0dbe9a2668b
takahiro-ueda <pt25ue@gmail.com>
2020-08-08 21:13:52 +0900

    InstantおよびZonedDateTimeの利用例

--- JavaLearn/src/lesson15/Main.java
+++ JavaLearn/src/lesson15/Main.java
@@ -1,65 +1,27 @@
 package lesson15;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.*;
 
 public class Main {
   public static void main(String[] args) throws Exception {
-    SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/ dd HH:mm:ss");
-    //文字列からDateインスタンスを生成
-    Date d = f.parse("2020/09/22 01:23:45");
-    System.out.println(d);
-    //Dateインスタンスから文字列を生成
-    Date now = new Date();
-    String s = f.format(now);
-    System.out.println("現在は" + s + "です！！");
+    // Instantの生成
+    Instant z1 = Instant.now(); //現在日時を取得
+    // Instantとlong値との相互変換
+    Instant i2 = Instant.ofEpochMilli(1600705425827L);
+    long l = i2.toEpochMilli();
 
-    // 処理時間を計測
-    // long start = System.currentTimeMillis();
-    // //ここで何らかの時間がかかる処理
-    // long end = System.currentTimeMillis();
-    // System.out.println("処理にかかった時間は・・・・" + (end - start) + "ミリ秒でした");
+    //ZonedDateTimeの生成
+    ZonedDateTime z1 = ZonedDateTime.now(); //現在日時を取得
+    ZonedDateTime z2 = ZonedDateTime.of(2020, 1, 2, 3, 4, 5, 6, ZoneId.of("Asia/Tokyo")); //「東京時間2020年1年2日3時4分5秒6ナノ秒」を指定して取得
 
-    // String s = "abc,def:ghi";
-    // String[] words = s.split("[,:]"); //正規表記パターン
-    // for (String w : words) {
-    //   System.out.println(w + "->");
-    // }
+    // InstantとZonedDateTimeの相互変換
+    Instant i3 = z2.toInstant();
+    ZonedDateTime z3 = i3.atZone(ZoneId.of("Europe/London"));
 
-    // String s = "abc,def:ghi";
-    // String w = s.replaceAll("[beh]","X");
-    // System.out.println(w);  // aXc,dXf:gXi
-
-    // StringBuilder sb = new StringBuilder();
-    // for (int i = 0; i < 10000; i++) {
-    //   sb.append("Java");  //①バッファにJavaを追加
-    // }
-    // String s = sb.toString(); //②完成した連結済み文字列を取り出す
-
-    // String s1 = "Java programming";
-    // System.out.println("文字列s1の4文字目以降は" + s1.substring(3));  // => a programming
-    // System.out.println("文字列s1の4~8文字目は" + s1.substring(3, 8)); // => a pro
-
-    // String s1 = "Java and JavaScript";
-    // if (s1.contains("Java")) {
-    //   System.out.println("文字列s1は、Javaを含んでいます");
-    // }
-    // if (s1.endsWith("Java")) {
-    //   System.out.println("文字列s1は、Javaが末尾にあります");
-    // }
-    // System.out.println("文字列s1で最初にJavaが登場する位置は" + s1.indexOf("Java"));
-    // System.out.println("文字列s1で最後にJavaが登場する位置は" + s1.lastIndexOf("Java"));
-
-    // String s2 = "Java";
-    // String s3 = "Java";
-    // if (s2.equals(s3)) {
-    //   System.out.println("s2とs3は等しい");
-    // }
-    // if (s2.equalsIgnoreCase(s3)) {
-    //   System.out.println("s2とs3はケースを区別しなければ等しい");
-    // }
-    // System.out.println("s1の長さは" + s1.length() + "です");  //全角文字も半角文字もz1文字としてカウント
-    // if (s1.isEmpty()) { //isEmptyはlength()==0より直感的に意味を捉えやすい
-    //   System.out.println("s1は空文字です！");
-    // }
+    //ZonedDateTimeの利用方法
+    System.out.println("東京" + z2.getYear() + z2.getMonth() + z2.getDayOfMonth());
+    System.out.println("ロンドン" + z3.getYear() + z3.getMonth() + z3.getDayOfMonth());
+    if (z2.isEqual(z3)) { //同じ瞬間の判定には、equals()ではなくisEqual()を使う
+      System.out.println("これらは同じ瞬間を指しています！");
+    }
   }
 }
