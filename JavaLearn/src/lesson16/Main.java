ce502d75031bd2b8c5d72e9b2f4268b7b89325fc
takahiro-ueda <pt25ue@gmail.com>
2020-08-08 22:00:07 +0900

    イテレータを使ったArrayListの繰り返し処理

--- JavaLearn/src/lesson16/Main.java
+++ JavaLearn/src/lesson16/Main.java
@@ -1,14 +1,16 @@
 package lesson16;
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
-    ArrayList<Integer> points = new ArrayList<Integer>(); //Integer型でArrayListを宣言
-    points.add(10);
-    points.add(80);
-    points.add(75);
-    for (int i : points) {  //拡張for文(4.4.5項)も利用可能
-      System.out.println(i);
+    ArrayList<String> names = new ArrayList<String>();
+    names.add("湊");
+    names.add("朝香");
+    names.add("菅原");
+    Iterator<String> it = names.iterator();
+    while (it.hasNext()) {
+      String e = it.next();
+      System.out.println(e);
     }
   }
 }
