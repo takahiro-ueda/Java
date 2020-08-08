2e0a98da382015bd0b8b280f5b5417623d203eef
takahiro-ueda <pt25ue@gmail.com>
2020-08-09 07:33:46 +0900

    Setから要素を取り出すと・・・

--- JavaLearn/src/lesson16/Main.java
+++ JavaLearn/src/lesson16/Main.java
@@ -5,10 +5,12 @@ import java.util.Set;
 public class Main {
   public static void main(String[] args) {
     Set<String> colors = new HashSet<String>();
+    //赤・青・黄の順に格納
     colors.add("赤");
     colors.add("青");
     colors.add("黄");
-    colors.add("赤"); //重複して「赤」を格納しようとしても無視される
-    System.out.println("色は" + colors.size() + "種類");
+    for(String s : colors) {
+      System.out.println(s + "→");
+    }
   }
 }
