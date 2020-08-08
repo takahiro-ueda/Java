35f8dc01949b8e78617bd071cda65f9ea395de19
takahiro-ueda <pt25ue@gmail.com>
2020-08-08 16:56:52 +0900

    現在日時を表示する

--- JavaLearn/src/lesson15/Main.java
+++ JavaLearn/src/lesson15/Main.java
@@ -1,11 +1,20 @@
 package lesson15;
+import java.util.Date;
 
 public class Main {
   public static void main(String[] args) {
-    long start = System.currentTimeMillis();
-    //ここで何らかの時間がかかる処理
-    long end = System.currentTimeMillis();
-    System.out.println("処理にかかった時間は・・・・" + (end - start) + "ミリ秒でした");
+    //現在日時を表示する
+    Date now = new Date();  //現在の日時を習得
+    System.out.println(now);
+    System.out.println(now.getTime());
+    Date past = new Date(1600705425827L);
+    System.out.println(past);
+
+    // 処理時間を計測
+    // long start = System.currentTimeMillis();
+    // //ここで何らかの時間がかかる処理
+    // long end = System.currentTimeMillis();
+    // System.out.println("処理にかかった時間は・・・・" + (end - start) + "ミリ秒でした");
 
     // String s = "abc,def:ghi";
     // String[] words = s.split("[,:]"); //正規表記パターン
