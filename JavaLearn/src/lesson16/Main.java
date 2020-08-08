3c84778a9bd3b3ab1c318c78e2adf7275c1d07c1
takahiro-ueda <pt25ue@gmail.com>
2020-08-09 08:21:50 +0900

    練習問題

--- JavaLearn/src/lesson16/Main.java
+++ JavaLearn/src/lesson16/Main.java
@@ -3,14 +3,25 @@ import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
-    Map<String, Integer> prefs = new HashMap<String, Integer>();
-     //ペアで値を格納
-    prefs.put("京都府", 255);
-    prefs.put("東京都", 1261);
-    prefs.put("熊本県", 182);
-    for (String key : prefs.keySet()) { //県名一覧を取得し繰り返す
-      int value = prefs.get(key); //県名(キー)を指定し人口(値)を取得
-      System.out.println(key + "の人口は、" + value);
-    }
+    //練習問題(Heroクラス)
+    // Hero h1 = new Hero("斎藤");
+    // Hero h2 = new Hero("鈴木");
+    // List<Hero> heroes = new ArrayList<Hero>();
+    // heroes.add(h1);
+    // heroes.add(h2);
+    // for (Hero h : heroes) {
+    //   System.out.println(h.getName());
+    // }
+
+    //練習問題
+    // Hero h1 = new Hero("斎藤");
+    // Hero h2 = new Hero("鈴木");
+    // Map<Hero, Integer> heroes = new HashMap<Hero, Integer>();
+    // heroes.put(h1, 3);
+    // heroes.put(h2, 7);
+    // for (Hero key : heroes.keySet()) {
+    //   int value = heroes.get(key);
+    //   System.out.println(key.getName + "が倒した敵＝" + value);
+    // }
   }
 }
