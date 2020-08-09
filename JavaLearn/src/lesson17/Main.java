81f68b1f9e0b1ad7212bd0deb2bf5b4cb99bcd3f
takahiro-ueda <pt25ue@gmail.com>
2020-08-09 11:23:48 +0900

    tryブロック内でnewすると・・・(エラー)

--- JavaLearn/src/lesson17/Main.java
+++ JavaLearn/src/lesson17/Main.java
@@ -4,11 +4,12 @@ import java.io.*;
 public class Main {
   public static void main(String[] args) {
     // FileWriter fw = null;
-    FileWriter fw = new FileWriter("data.txt"); //mainメソッドブロック(5~12行目)をfwのスコープにする
+    FileWriter fw;  //mainメソッドブロック(5~13行目)はfwのスコープのまま
     try {
+      fw = new FileWriter("data.txt");  //tryブロック内でコンストラクタが動作する
       fw.write("hello!"); //スコープ内だからfwを利用可能！
     } catch (IOException e) { 
-      System.out.println("何らかのエラーが発生する"); //例外的状況になったときに備えて記述された代替処理
+      System.out.println("何らかの例外が発生しました"); //例外的状況になったときに備えて記述された代替処理
     } finally {
       fw.close(); //スコープ内だからfwを利用可能！
     }
