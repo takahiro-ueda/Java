e198b93d710842a67664ffe8a7ca677a79b4c57c
takahiro-ueda <pt25ue@gmail.com>
2020-08-09 11:53:14 +0900

    例外インスタンスを自分で投げる

--- JavaLearn/src/lesson17/Main.java
+++ JavaLearn/src/lesson17/Main.java
@@ -1,13 +1,9 @@
 package lesson17;
-import java.io.*;
+// import java.io.*;
 
 public class Main {
   public static void main(String[] args) {
-    // FileWriter fw = null;
-    try (FileWriter fw = new FileWriter("data.txt");) { //tryブロック内でコンストラクタが動作する
-      fw.write("hello!"); //スコープ内だからfwを利用可能！
-    } catch (IOException e) { 
-      System.out.println("何らかの例外が発生しました"); //例外的状況になったときに備えて記述された代替処理
-    }
+    Person p = new Person();
+    p.setAge(-128); //誤った値のセットを試みる→例外発生
   }
 }
