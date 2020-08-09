29efabb7f76f7af4324cdf3e1407c2b6c63e1961
takahiro-ueda <pt25ue@gmail.com>
2020-08-09 11:19:49 +0900

    変数fwのスコープを広げると・・・(エラー)

--- JavaLearn/src/lesson17/Main.java
+++ JavaLearn/src/lesson17/Main.java
@@ -4,13 +4,13 @@ import java.io.*;
 public class Main {
   public static void main(String[] args) {
     // FileWriter fw = null;
+    FileWriter fw = new FileWriter("data.txt"); //mainメソッドブロック(5~12行目)をfwのスコープにする
     try {
-      FileWriter fw = new FileWriter("data.txt");
-      fw.write("hello!");
+      fw.write("hello!"); //スコープ内だからfwを利用可能！
     } catch (IOException e) { 
       System.out.println("何らかのエラーが発生する"); //例外的状況になったときに備えて記述された代替処理
     } finally {
-      fw.close(); //この行でエラー
+      fw.close(); //スコープ内だからfwを利用可能！
     }
   }
 }
