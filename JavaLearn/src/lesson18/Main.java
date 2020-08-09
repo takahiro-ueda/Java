dda308f4803baa9a93e39ee6c3f66f595efa3ceb
takahiro-ueda <pt25ue@gmail.com>
2020-08-09 13:18:12 +0900

    はじめてのGUIプログラム

--- JavaLearn/src/lesson18/Main.java
+++ JavaLearn/src/lesson18/Main.java
@@ -1,13 +1,17 @@
 package lesson18;
-import java.sql.*;
+import java.awt.FlowLayout;
+import javax.swing.*;
 
 public class Main {
-  public static void main(String[] args) throws Exception {
-    Class.forName("org.h2.Driver");
-    String dburl = "jdbc:h2:~/test";  //接続先DBを指定
-    String sql = "INSERT INTO EMPLOYESS(name) VALUES('aoki')";
-    Connection conn = DriverManager.getConnection(dburl); //DBに接続
-    conn.createStatement().executeUpdate(sql);  //sqlを送信
-    conn.close(); //db接続を閉じる
+  public static void main(String[] args) {
+    JFrame frame = new JFrame("はじめてのGUI");
+    JLabel label = new JLabel("Hello World");
+    JButton button = new JButton("押してね！");
+    frame.getContentPane().setLayout(new FlowLayout());
+    frame.getContentPane().add(label);
+    frame.getContentPane().add(button);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(300, 100);
+    frame.setVisible(true);
   }
 }
