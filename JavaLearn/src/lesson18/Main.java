ffd0d44261ce7349433d0a34a6fd0d179ac6e8f2
takahiro-ueda <pt25ue@gmail.com>
2020-08-09 12:45:14 +0900

    ファイルに1文字ずつ書き込む

--- JavaLearn/src/lesson18/Main.java
+++ JavaLearn/src/lesson18/Main.java
@@ -3,12 +3,12 @@ import java.io.*;
 
 public class Main {
   public static void main(String[] args) throws Exception {
-    FileReader fr = new FileReader("data.txt"); //ファイルを開く
-    int input = fr.read();
-    while (input != -1) {
-      System.out.println((char)input);
-      input = fr.read();
-    }
-    fr.close(); //ファイルを閉じる
+    // FileReader fr = new FileReader("data.txt"); //ファイルを開く
+    FileWriter fw = new FileWriter("data.txt"); //ファイルを開く
+    fw.write('そ');
+    fw.write('れ');
+    fw.write('で');
+    fw.write('は');
+    fw.close(); //ファイルを閉じる
   }
 }
