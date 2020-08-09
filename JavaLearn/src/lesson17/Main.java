16ea1aa27b32cbec5fc2dff76e69dd5ce2a99ec1
takahiro-ueda <pt25ue@gmail.com>
2020-08-09 11:33:30 +0900

    後片付け処理もtry-catchする

--- JavaLearn/src/lesson17/Main.java
+++ JavaLearn/src/lesson17/Main.java
@@ -3,15 +3,19 @@ import java.io.*;
 
 public class Main {
   public static void main(String[] args) {
-    // FileWriter fw = null;
-    FileWriter fw;  //mainメソッドブロック(5~13行目)はfwのスコープのまま
+    FileWriter fw = null;
+    // FileWriter fw;  //mainメソッドブロック(5~13行目)はfwのスコープのまま
     try {
       fw = new FileWriter("data.txt");  //tryブロック内でコンストラクタが動作する
       fw.write("hello!"); //スコープ内だからfwを利用可能！
     } catch (IOException e) { 
       System.out.println("何らかの例外が発生しました"); //例外的状況になったときに備えて記述された代替処理
     } finally {
-      fw.close(); //スコープ内だからfwを利用可能！
+      try {
+        fw.close();
+      } catch (IOException e) {
+        ;
+      }
     }
   }
 }
