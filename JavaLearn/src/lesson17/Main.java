b1dec2da22342efc8982ab8777c18cf57da1ec8f
takahiro-ueda <pt25ue@gmail.com>
2020-08-09 11:05:23 +0900

    ザックリと例外を補足する

--- JavaLearn/src/lesson17/Main.java
+++ JavaLearn/src/lesson17/Main.java
@@ -5,8 +5,10 @@ public class Main {
   public static void main(String[] args) {
     try {
       FileWriter fw = new FileWriter("data.txt");
-    } catch (IOException e) {
-      System.out.println("エラーが発生しました！"); //例外的状況になったときに備えて記述された代替処理
+      fw.write("hello!");
+      fw.close();
+    } catch (IOException e) { //Exceptionの子孫をどれでもキャッチ
+      System.out.println("何らかの例外が発生しました"); //例外的状況になったときに備えて記述された代替処理
     }
   }
 }
