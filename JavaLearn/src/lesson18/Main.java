cb3598013453a105d447ad076a9823a5e41c1d4f
takahiro-ueda <pt25ue@gmail.com>
2020-08-09 13:04:27 +0900

    DBを接続してSQLを送信する

--- JavaLearn/src/lesson18/Main.java
+++ JavaLearn/src/lesson18/Main.java
@@ -1,16 +1,13 @@
 package lesson18;
-import java.io.InputStream;
-import java.net.URL;
+import java.sql.*;
 
 public class Main {
   public static void main(String[] args) throws Exception {
-    URL u = new URL("https://book.impress.co.jp/");
-    InputStream is = u.openStream();  //インターネットへ接続
-    int i = is.read();
-    while (i != -1) { //ページの終わりまで繰り返す
-      char c = (char)i;
-      System.out.print(c);  //読んだ内容を画面に表示
-      i = is.read();
-    }
+    Class.forName("org.h2.Driver");
+    String dburl = "jdbc:h2:~/test";  //接続先DBを指定
+    String sql = "INSERT INTO EMPLOYESS(name) VALUES('aoki')";
+    Connection conn = DriverManager.getConnection(dburl); //DBに接続
+    conn.createStatement().executeUpdate(sql);  //sqlを送信
+    conn.close(); //db接続を閉じる
   }
 }
